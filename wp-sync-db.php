2117d096949fdf31ae73d1749b5f658d085a8b17
Sean Lang <slang800@gmail.com>
2016-03-07 00:48:08 -0500

    apply PSR1 & PSR2 formatting

--- wp-sync-db.php
+++ wp-sync-db.php
@@ -7,40 +7,45 @@ Version: 1.5
 Author URI: http://slang.cx
 GitHub Plugin URI: wp-sync-db/wp-sync-db
 Network: True
-*/
+ */
 
 $GLOBALS['wpsdb_meta']['wp-sync-db']['version'] = '1.5';
-$GLOBALS['wpsdb_meta']['wp-sync-db']['folder'] = basename( plugin_dir_path( __FILE__ ) );
+$GLOBALS['wpsdb_meta']['wp-sync-db']['folder'] = basename(plugin_dir_path(__FILE__));
 
 // Define the directory seperator if it isn't already
-if( !defined( 'DS' ) ) {
+if (!defined('DS')) {
     if (strtoupper(substr(PHP_OS, 0, 3)) == 'WIN') {
         define('DS', '\\');
-    }
-    else {
+    } else {
         define('DS', '/');
     }
 }
 
-function wp_sync_db_loaded() {
+function wp_sync_db_loaded()
+{
     // if neither WordPress admin nor running from wp-cli, exit quickly to prevent performance impact
-    if ( !is_admin() && ! ( defined( 'WP_CLI' ) && WP_CLI ) ) return;
+    if (!is_admin() && !(defined('WP_CLI') && WP_CLI)) {
+        return;
+    }
 
     require_once 'class/wpsdb-base.php';
     require_once 'class/wpsdb-addon.php';
     require_once 'class/wpsdb.php';
 
     global $wpsdb;
-    $wpsdb = new WPSDB( __FILE__ );
+    $wpsdb = new WPSDB(__FILE__);
 }
 
-add_action( 'plugins_loaded', 'wp_sync_db_loaded' );
+add_action('plugins_loaded', 'wp_sync_db_loaded');
 
-function wp_sync_db_init() {
+function wp_sync_db_init()
+{
     // if neither WordPress admin nor running from wp-cli, exit quickly to prevent performance impact
-    if ( !is_admin() && ! ( defined( 'WP_CLI' ) && WP_CLI ) ) return;
+    if (!is_admin() && !(defined('WP_CLI') && WP_CLI)) {
+        return;
+    }
 
-    load_plugin_textdomain( 'wp-sync-db', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+    load_plugin_textdomain('wp-sync-db', false, dirname(plugin_basename(__FILE__)) . '/languages/');
 }
 
-add_action( 'init', 'wp_sync_db_init' );
+add_action('init', 'wp_sync_db_init');
