dbc29d7198fadd74083fb0098e12e343b1df5471
Sean Lang <slang800@gmail.com>
2016-03-07 00:19:03 -0500

    switch from tabs to spaces

--- compatibility/wp-sync-db-compatibility.php
+++ compatibility/wp-sync-db-compatibility.php
@@ -16,16 +16,16 @@ $GLOBALS['wpsdb_compatibility'] = true;
 * @return array
 */
 function wpsdbc_exclude_plugins( $plugins ) {
-	if ( !defined( 'DOING_AJAX' ) || !DOING_AJAX || !isset( $_POST['action'] ) || false === strpos( $_POST['action'], 'wpsdb' ) ) return $plugins;
-	$wpsdb_settings = get_option( 'wpsdb_settings' );
-	if ( !empty( $wpsdb_settings['blacklist_plugins'] ) ) {
-		$blacklist_plugins = array_flip( $wpsdb_settings['blacklist_plugins'] );
-	}
-	foreach( $plugins as $key => $plugin ) {
-		if ( false !== strpos( $plugin, 'wp-sync-db' ) || !isset( $blacklist_plugins[$plugin] ) ) continue;
-		unset( $plugins[$key] );
-	}
-	return $plugins;
+    if ( !defined( 'DOING_AJAX' ) || !DOING_AJAX || !isset( $_POST['action'] ) || false === strpos( $_POST['action'], 'wpsdb' ) ) return $plugins;
+    $wpsdb_settings = get_option( 'wpsdb_settings' );
+    if ( !empty( $wpsdb_settings['blacklist_plugins'] ) ) {
+        $blacklist_plugins = array_flip( $wpsdb_settings['blacklist_plugins'] );
+    }
+    foreach( $plugins as $key => $plugin ) {
+        if ( false !== strpos( $plugin, 'wp-sync-db' ) || !isset( $blacklist_plugins[$plugin] ) ) continue;
+        unset( $plugins[$key] );
+    }
+    return $plugins;
 }
 add_filter( 'option_active_plugins', 'wpsdbc_exclude_plugins' );
 
@@ -36,15 +36,15 @@ add_filter( 'option_active_plugins', 'wpsdbc_exclude_plugins' );
 * @return array
 */
 function wpsdbc_exclude_site_plugins( $plugins ) {
-	if ( !defined( 'DOING_AJAX' ) || !DOING_AJAX || !isset( $_POST['action'] ) || false === strpos( $_POST['action'], 'wpsdb' ) ) return $plugins;
-	$wpsdb_settings = get_option( 'wpsdb_settings' );
-	if ( !empty( $wpsdb_settings['blacklist_plugins'] ) ) {
-		$blacklist_plugins = array_flip( $wpsdb_settings['blacklist_plugins'] );
-	}
-	foreach( array_keys( $plugins ) as $plugin ) {
-		if ( false !== strpos( $plugin, 'wp-sync-db' ) || !isset( $blacklist_plugins[$plugin] ) ) continue;
-		unset( $plugins[$plugin] );
-	}
-	return $plugins;
+    if ( !defined( 'DOING_AJAX' ) || !DOING_AJAX || !isset( $_POST['action'] ) || false === strpos( $_POST['action'], 'wpsdb' ) ) return $plugins;
+    $wpsdb_settings = get_option( 'wpsdb_settings' );
+    if ( !empty( $wpsdb_settings['blacklist_plugins'] ) ) {
+        $blacklist_plugins = array_flip( $wpsdb_settings['blacklist_plugins'] );
+    }
+    foreach( array_keys( $plugins ) as $plugin ) {
+        if ( false !== strpos( $plugin, 'wp-sync-db' ) || !isset( $blacklist_plugins[$plugin] ) ) continue;
+        unset( $plugins[$plugin] );
+    }
+    return $plugins;
 }
 add_filter( 'site_option_active_sitewide_plugins', 'wpsdbc_exclude_site_plugins' );
