4f22305db2ea92640e8f2c0022069263a30664cf
Sean Lang <slang800@gmail.com>
2016-03-07 02:22:32 -0500

    add test for PSR1/2 formatting

--- test.sh
+++ test.sh
@@ -6,4 +6,6 @@ files=(wp-sync-db.php class/*.php compatibility/*.php template/*.php);
 for file in "${files[@]}"
 do
   php -l "$file";
+  php vendor/phpfmt/fmt/fmt.phar --psr1 --psr2 -o=- "$file" | diff -U3 \
+  --label "$file (correct formatting)" --label "$file (original)" - "$file";
 done
